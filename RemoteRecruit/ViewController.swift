ac015dd554ab093b0f326305f4118d554c9339ec
Alagarsamy <alagarsamy825@gmail.com>
2026-06-11 21:13:28 +0530

    Initial Commit

---
+++ RemoteRecruit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemoteRecruit
+//
+//  Created by Alagarsamy on 11/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
